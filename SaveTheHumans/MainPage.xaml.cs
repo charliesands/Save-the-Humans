1f5d765dba72bda1ccb1e1d01f654aa76a2af69d
charliesands <cwsands3@gmail.com>
2018-10-31 19:14:33 -0600

    game works needs styling

--- SaveTheHumans/MainPage.xaml.cs
+++ SaveTheHumans/MainPage.xaml.cs
@@ -210,14 +210,15 @@ namespace SaveTheHumans
             }
         }
 
-        private void grid_PointerMoved(object sender, PointerRoutedEventArgs e)
+        private void playArea_PointerMoved(object sender, PointerRoutedEventArgs e)
         {
             if (humanCaptured)
             {
                 Point pointerPosition = e.GetCurrentPoint(null).Position;
                 Point relativePosition = grid.TransformToVisual(playArea).TransformPoint(pointerPosition);
-                if ((Math.Abs(relativePosition.X - Canvas.GetLeft(human)) > human.ActualWidth * 3)
-                    || (Math.Abs(relativePosition.Y - Canvas.GetTop(human)) > human.ActualHeight * 3))
+
+                if ((Math.Abs(relativePosition.X - Canvas.GetLeft(human)) > human.ActualWidth * 3) ||
+                    (Math.Abs(relativePosition.Y - Canvas.GetTop(human)) > human.ActualHeight * 3))
                 {
                     humanCaptured = false;
                     human.IsHitTestVisible = true;
@@ -230,7 +231,7 @@ namespace SaveTheHumans
             }
         }
 
-        private void grid_PointerExited(object sender, PointerRoutedEventArgs e)
+        private void playArea_PointerExited(object sender, PointerRoutedEventArgs e)
         {
             if (humanCaptured)
                 EndTheGame();
